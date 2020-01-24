b23e0913b1b46591511c774dfe811774cc8a3626
Jack Ryan Oracoy <jackryanoracoy@gmail.com>
2020-01-24 16:58:34 +0800

    Made minor fixes.

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -14,7 +14,8 @@ window.onload = function(){
   var anchors = document.getElementsByTagName('a');
   for (var i=0; i<anchors.length; i++){
     if (anchors[i].hostname != window.location.hostname) {
-      anchors[i].setAttribute('target', '_blank'), ('rel', 'noreferrer noopener');
+      anchors[i].setAttribute('target', '_blank');
+      anchors[i].setAttribute('rel', 'noreferrer noopener');
     }
   }
 }
